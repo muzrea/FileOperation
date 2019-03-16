3e750a84547b549476e63dbe30fa3927163d6cef
muzrea <muzrea@hotmail.com>
2019-03-16 17:04:33 +0800

    create copyFolder method

--- src/file_operation/CopyFiles.java
+++ src/file_operation/CopyFiles.java
@@ -26,4 +26,41 @@ public class CopyFiles {
             }
         }
     }
+
+    public void copyFolder(String targetFilePath, String copyFilePath) {
+        try {
+            File copyFile = new File(copyFilePath);
+            copyFile.mkdirs();
+            File targetFile = new File(targetFilePath);
+            String[] files = targetFile.list();
+            File temp = null;
+            for (int i = 0; i < files.length; i++) {
+                if (targetFilePath.endsWith(File.separator)) {
+                    temp = new File(targetFilePath + files[i]);
+                } else {
+                    temp = new File(targetFilePath + File.separator + files[i]);
+                }
+                if (temp.isFile()) {
+                    FileInputStream inputStream = new FileInputStream(temp);
+                    FileOutputStream outputStream = new FileOutputStream(copyFilePath + "/" + (temp.getName()).toString());
+                    byte[] newByteData = new byte[1024 * 5];
+                    byte text = 0;
+                    int len;
+                    while ((len = inputStream.read(newByteData)) != -1) {
+                        outputStream.write(newByteData, 0, len);
+                    }
+                    outputStream.flush();
+                    outputStream.close();
+                    inputStream.close();
+                }
+                if (temp.isDirectory()) {
+                    copyFolder(targetFilePath + "/" + files[i], copyFilePath + "/" + files[i]);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("复制文件夹出错");
+            e.printStackTrace();
+        }
+    }
 }
